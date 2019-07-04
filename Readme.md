e6810a2555c1d99290c9349f1c0180f8af31abc4
muscaiu <muscaiu@yahoo.com>
2019-07-04 08:23:34 +0300

    made first book request

---
+++ Readme.md
@@ -0,0 +1,9 @@
+http://localhost:5001/graphql
+
+{
+  book(id: "3") {
+    name
+    genre
+    id
+  }
+}
---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}));
+
+app.listen('5001', () => console.log('App running on 5001'));
