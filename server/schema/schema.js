a9d8666b4326a85f7479a15b6f7ed972eee57c55
muscaiu <muscaiu@yahoo.com>
2019-07-24 22:26:09 +0300

    introducing relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,9 +10,9 @@ const {
 
 // dummy data
 const books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
 ];
 const authors = [
   { name: 'Patrick Rothfuss', age: 44, id: '1' },
@@ -26,6 +26,13 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        console.log(parent)
+        return find(authors, { id: parent.authorId });
+      }
+    }
   })
 });
 
