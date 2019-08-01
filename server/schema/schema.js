2068871291acdbe12f68ead5a9e5bb7957274550
muscaiu <muscaiu@yahoo.com>
2019-08-01 08:24:20 +0300

    more mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -14,17 +14,17 @@ const {
 
 // dummy data
 // const books = [
-//   { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
-//   { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
-//   { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
-//   { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
-//   { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
-//   { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' },
+//   { name: "Name of the Wind", genre: "Fantasy",  authorId: "5d39cfd6cf08c317b853f64e" },
+//   { name: "The Final Empire", genre: "Fantasy",  authorId: "5d39cfe6cf08c317b853f64f" },
+//   { name: "The Hero of Ages", genre: "Fantasy", authorId: "5d39cfe6cf08c317b853f64f" },
+//   { name: "The Long Earth", genre: "Sci-Fi", authorId: "5d39cff5cf08c317b853f650" },
+//   { name: "The Colour of Magic", genre: "Fantasy",  authorId: "5d39cff5cf08c317b853f650" },
+//   { name: "The Light Fantastic", genre: "Fantasy", authorId: "5d39cff5cf08c317b853f650" },
 // ];
 // const authors = [
-//   { name: 'Patrick Rothfuss', age: 44, id: '1' },
-//   { name: 'Brandon Sanderson', age: 42, id: '2' },
-//   { name: 'Terry Pratchett', age: 66, id: '3' }
+// { name: "Patrick Rothfuss", age: 44, id: "1" },
+// { name: "Brandon Sanderson", age: 42, id: "2" },
+// { name: "Terry Pratchett", age: 66, id: "3" }
 // ];
 
 const BookType = new GraphQLObjectType({
@@ -107,6 +107,22 @@ const Mutation = new GraphQLObjectType({
           age: args.age
         });
         return author.save();
+      }
+    },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID }
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId
+        });
+        return book.save();
       }
     }
   }
